6687394ea1395bf7114daa936ade4dd381a9efe2
Elizabeth Smith <elizabethannsmith.es@gmail.com>
2023-02-14 11:37:27 +0000

    Using if statement

---
+++ learnpython.org/Conditions/ifStatements.py
@@ -0,0 +1,12 @@
+# Using if statements
+statement = False
+another_statement = True
+if statement is True:
+    print("statement is True")
+    pass
+elif another_statement is True: 
+    print("another_statment is True")
+    pass
+else:
+    print("Neither is True")
+    pass 

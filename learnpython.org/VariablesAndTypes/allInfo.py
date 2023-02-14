d4b9ba3059546ebe6d470eec63cb5cd31c582647
Elizabeth Smith <elizabethannsmith.es@gmail.com>
2023-02-14 08:43:09 +0000

    Concatenate all tutorial info into one file

---
+++ learnpython.org/VariablesAndTypes/allInfo.py
@@ -0,0 +1,48 @@
+# Set and print an integer
+myInt = 7
+print(myInt)
+
+
+# Define and print a floating point number
+myFloat = 7.0
+print(myFloat)
+myFloat = float(7)
+print(myFloat)
+
+
+# Print a string
+myString = 'hello'
+print(myString)
+myString = "hello again"
+print(myString)
+
+
+# Don't worry about apostrophes
+myString = "Don't worry about apostrophes"
+print (myString)
+
+
+# Learn about simple operators
+one = 1
+two = 2
+three = 1 + 2
+print(three)
+
+hello = "hello"
+world = "world"
+helloworld = hello + " " + world
+print(helloworld)
+
+
+# Learn about assignments
+a, b = 3, 4
+print(a, b)
+
+
+# Mixing operators doesn't work
+# This will not work
+one = 1
+two = 2
+hello = "hello"
+
+print(one + two + hello)

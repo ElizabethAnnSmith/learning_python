8b34051183b1a4ccc9b8ec40b3aa73e592575b2e
Elizabeth Ann Smith <47785247+ElizabethAnnSmith@users.noreply.github.com>
2023-02-14 09:35:14 +0000

    Update allinfo.py

--- learnpython.org/BasicOperators/allinfo.py
+++ learnpython.org/BasicOperators/allinfo.py
@@ -1,24 +1,36 @@
 # Learn arithmetic operators
 number = 1 + 2 * 3 / 4.0
 print(number)
+
+
 # Modulo (percentage %) operator
 remainder = 11 % 3
 print(remainder)
+
+
 # Using two multiplication operators
 squared = 7 ** 2
 cubed = 2 ** 3
 print(squared)
 print(cubed)
+
+
 # Concatenate strings with addition operator
 helloworld = "hello" + " " + "world"
 print(helloworld)
+
+
 # Using the multiplication operator with strings
 lotsOfHellos = "hello" * 10
 print(lotsOfHellos)
+
+
 # Joining lists with the addition operator
 even = [2,4,6,8]
 odd = [1,3,5,7]
 all = odd + even
 print(all)
+
+
 # Repeat a list with multiplication operator"
 print([1,2,3] * 3)

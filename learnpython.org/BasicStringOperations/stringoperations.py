1f815345a46f9aacd40382cc180bdb88ffc5d458
Elizabeth Smith <elizabethannsmith.es@gmail.com>
2023-02-14 10:31:28 +0000

    Strings and quotes

---
+++ learnpython.org/BasicStringOperations/stringoperations.py
@@ -0,0 +1,9 @@
+# What can do with strings
+
+# Quotations don't matter for strings
+astring = "Hello World!"
+astring2 = 'Hello World!'
+
+# To assign a string in a variable already containing single brackets, you need to use double brackets
+print("single quotes are ' '")
+print(len(astring))

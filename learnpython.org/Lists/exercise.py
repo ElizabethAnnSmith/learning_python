9e6f3c7fa3749e85c9d1e1c6845d0442059d08de
Elizabeth Smith <elizabethannsmith.es@gmail.com>
2023-02-14 09:07:25 +0000

    Tutorial exercise

---
+++ learnpython.org/Lists/exercise.py
@@ -0,0 +1,20 @@
+# Tutorial exercise
+
+# Add numbers and strings to correct list using append method.
+# Add numbers 1, 2 and 3 to numbers list
+# Add words 'hello' and 'world' to strings list
+# Fill in second_name list using brackets operator []
+
+numbers = [1, 2, 3]
+strings = ["hello", "world"]
+names = ["John", "Eric", "Jessica"]
+second_name = ["Smith", "Daniel", "Stone"]
+
+# Print the arrays
+print(numbers)
+print(strings)
+print(names)
+print(second_name)
+
+# Print the second name on the names lists
+print("The second name on the names list is %s %s" % (names[1], second_name[1]))

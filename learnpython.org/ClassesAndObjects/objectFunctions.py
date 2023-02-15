94aaab3f4d325486cad385fc68576631067e0aa1
Elizabeth Smith <elizabethannsmith.es@gmail.com>
2023-02-15 11:27:51 +0000

    Learn to access funtions inside objects

---
+++ learnpython.org/ClassesAndObjects/objectFunctions.py
@@ -0,0 +1,10 @@
+# Learn to access functions inside classes
+class MyClass:
+    variable = "blah"
+
+    def function(self):
+        print("This is a message inside the class.")
+
+myobjectx = MyClass()
+
+myobjectx.function()

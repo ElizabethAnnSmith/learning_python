dd099cfa01ed525da70bcfd766988356d3cabc1c
Elizabeth Smith <elizabethannsmith.es@gmail.com>
2023-02-15 11:46:10 +0000

    Learn to create and populate a dictionary

---
+++ learnpython.org/Dictionaries/firstDictionary.py
@@ -0,0 +1,14 @@
+# Learn to create and populate a dictionary
+phonebook = {}
+phonebook["John"] = 938477566
+phonebook["Jack"] = 938377264
+phonebook["Jill"] = 947662781
+print(phonebook)
+
+# Another way to define it is...
+phonebook = {
+    "John" : 938477566,
+    "Jack" : 938377264,
+    "Jill" : 947662781
+}
+print(phonebook)

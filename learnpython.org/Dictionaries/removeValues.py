d0e08ebe043455a89927955213ec4178da1155a7
Elizabeth Smith <elizabethannsmith.es@gmail.com>
2023-02-15 11:50:38 +0000

    And another way of removing values

--- learnpython.org/Dictionaries/removeValues.py
+++ learnpython.org/Dictionaries/removeValues.py
@@ -6,3 +6,12 @@ phonebook = {
 }
 del phonebook["John"]
 print(phonebook)
+
+# Or ...
+phonebook = {
+   "John" : 938477566,
+   "Jack" : 938377264,
+   "Jill" : 947662781
+}
+phonebook.pop("John")
+print(phonebook)

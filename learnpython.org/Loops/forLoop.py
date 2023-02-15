d6872b3227b0a6b215be57302db375f2b31cf6fe
Elizabeth Smith <elizabethannsmith.es@gmail.com>
2023-02-15 10:20:01 +0000

    Learn about for loops

---
+++ learnpython.org/Loops/forLoop.py
@@ -0,0 +1,18 @@
+# Learn about for loops - iterate using 'range' and 'xrange' functions
+# Range returns a new list with numbers of specified range
+# Xrange returns an iterator - more efficient
+primes = [2, 3, 5, 7]
+for prime in primes:
+    print(prime)
+
+# Prints out the numbers 0,1,2,3,4
+for x in range(5):
+    print(x)
+
+# Prints out 3,4,5
+for x in range(3, 6):
+    print(x)
+
+# Prints out 3,5,7
+for x in range(3, 8, 2):
+    print(x)

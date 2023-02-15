24137c23c96f2bbcd047611a4aac0655afe87908
Elizabeth Smith <elizabethannsmith.es@gmail.com>
2023-02-15 11:24:24 +0000

    Learn to create and use classes

---
+++ learnpython.org/ClassesAndObjects/firstClass.py
@@ -0,0 +1,13 @@
+# A basic class
+class MyClass:
+    variable = "blah"
+
+    def function(self):
+        print("This is a message inside the class.")
+
+myobjectx = MyClass()
+
+# To access 'variable' we need to use
+# myobjectx.variable
+# for example
+print(myobjectx.variable)

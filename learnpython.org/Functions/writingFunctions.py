c9737fd7020ed172d58d82a90ecfece3867cebe6
Elizabeth Smith <elizabethannsmith.es@gmail.com>
2023-02-15 10:51:04 +0000

    Learning to write a function

---
+++ learnpython.org/Functions/writingFunctions.py
@@ -0,0 +1,12 @@
+# Learning to write a function
+block_head:
+    1st block line
+    2nd block line
+    ...
+# block_line is python code or another block
+# block_head is has this format: block_keyword block_name(arguement1, arguement2, ...)
+# Examples of block_keywords are 'if', 'for' and 'while'
+# Functions are defined using the block_keyword 'def' followed by a name
+
+def my_function():
+    print("Hello From My Function!")

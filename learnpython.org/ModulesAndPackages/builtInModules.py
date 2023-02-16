1a14c8bff43f029b058fe71a3e668cc3cf361f48
Elizabeth Smith <elizabethannsmith.es@gmail.com>
2023-02-16 15:52:09 +0000

    Using built in modules

---
+++ learnpython.org/ModulesAndPackages/builtInModules.py
@@ -0,0 +1,8 @@
+# Full list of built-in modules in the Python standard library
+# Helpful functions - 'dir' and 'help'
+# To import the module 'urllib', which enables us to create read data from URLs, we 'import' the module:
+# import the library
+import urllib
+
+# use it
+urllib.urlopen(...)

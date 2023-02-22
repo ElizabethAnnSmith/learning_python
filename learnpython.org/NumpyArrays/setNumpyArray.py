ee1ad7b6f9b42748421d6b14d848d769435ef545
Elizabeth Smith <elizabethannsmith.es@gmail.com>
2023-02-22 17:49:31 +0000

    Set up Numpy Arrays

---
+++ learnpython.org/NumpyArrays/setNumpyArray.py
@@ -0,0 +1,14 @@
+# Learn about Numpy Arrays
+# Great alternative to Python Lists. 
+# Advantages are that they are fast, easy to work with and allow users to perform calculations across arrays.
+
+# Create 2 new lists height and weight
+height = [1.87,  1.87, 1.82, 1.91, 1.90, 1.85]
+weight = [81.65, 97.52, 95.25, 92.98, 86.18, 88.45]
+
+# Import the numpy package as np
+import numpy as np
+
+# Create 2 numpy arrays from height and weight
+np_height = np.array(height)
+np_weight = np.array(weight)
